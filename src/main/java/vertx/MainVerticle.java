a6013d5142a3f6f03e2abe2d5c1f71663f56f9d4
mariela-radeva <65311051+mariela-radeva@users.noreply.github.com>
2020-05-14 23:37:13 +0300

    Using Vertx-Web

--- src/main/java/vertx/MainVerticle.java
+++ src/main/java/vertx/MainVerticle.java
@@ -2,12 +2,35 @@ package vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
+    Router router = Router.router(vertx);
+      router.route("/hello").handler(StaticHandler.create("client"));
+
+      router.route("/hello").handler(routingContext -> {
+          HttpServerResponse response = routingContext.response();
+          System.out.println("Hello");
+          response.sendFile("argon-dashboard-master/examples/login.html");
+      });
+
+      vertx.createHttpServer().requestHandler(router).listen(8888, http -> {
+        if (http.succeeded()) {
+          startPromise.complete();
+          System.out.println("HTTP server started on port 8888");
+        } else {
+          startPromise.fail(http.cause());
+        }
+      });
+
+
+
+    /*vertx.createHttpServer().requestHandler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
         .end("Hello from Vert.x!");
@@ -18,6 +41,6 @@ public class MainVerticle extends AbstractVerticle {
       } else {
         startPromise.fail(http.cause());
       }
-    });
+    });*/
   }
 }
