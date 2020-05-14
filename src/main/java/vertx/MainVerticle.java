61869196278ae4ebf159466e688265a84985960b
mariela-radeva <65311051+mariela-radeva@users.noreply.github.com>
2020-05-14 21:37:31 +0300

    Recreate project

--- src/main/java/vertx/MainVerticle.java
+++ src/main/java/vertx/MainVerticle.java
@@ -2,42 +2,22 @@ package vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.StaticHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start(Promise<Void> promise) throws Exception {
-    Router router = Router.router(vertx);
-    router.route("/login").handler(StaticHandler.create("client"));
-
-    router.route("/login").handler(routingContext -> {
-      HttpServerResponse response = routingContext.response();
-      response.sendFile("argon-dashboard-master/examples/login.html");
-    });
-
-    vertx.createHttpServer().requestHandler(router).listen(8888, http -> {
-      if(http.succeeded()) {
-        promise.complete();
-        System.out.println("Server started on port 8888");
-      } else {
-        promise.fail(http.cause());
-      }
-    });
-
-
-    /*vertx.createHttpServer().requestHandler(req -> {
-      req.response().putHeader("content-type", "text/plain").end("Hello");
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
     }).listen(8888, http -> {
-      if(http.succeeded()){
-        promise.complete();
-        System.out.println("Server started on port 8888");
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
       } else {
-        promise.fail(http.cause());
+        startPromise.fail(http.cause());
       }
-    });*/
-
+    });
   }
 }
--- src/test/java/vertx/TestMainVerticle.java
+++ src/test/java/vertx/TestMainVerticle.java
@@ -1,6 +1,5 @@
 package vertx;
 
-import io.vertx.core.Verticle;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
@@ -8,12 +7,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
 
   @BeforeEach
   void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle((Verticle) new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
   }
 
   @Test
